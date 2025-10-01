53b826538ac394118fc0fef964d05b112685a52a
sweetpotatotest <kgb4232@daum.net>
2025-10-01 21:03:57 +0900

    feat: toss CSS 수정중

--- src/features/payment/pages/FailPage.tsx
+++ src/features/payment/pages/FailPage.tsx
@@ -1,27 +1,35 @@
-import {useSearchParams} from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
+import '../Toss.css'
 
 export function FailPage() {
   const [searchParams] = useSearchParams();
 
   return (
-    <div id="info" className="box_section" style={{ width: "600px" }}>
-      <img width="100px" src="https://static.toss.im/lotties/error-spot-no-loop-space-apng.png" alt="에러 이미지" />
+    <div id="info" className="toss_box_section">
+      <img
+        width="100px"
+        src="https://static.toss.im/lotties/error-spot-no-loop-space-apng.png"
+        alt="에러 이미지"
+      />
       <h2>결제를 실패했어요</h2>
 
-      <div className="p-grid typography--p" style={{ marginTop: "50px" }}>
-        <div className="p-grid-col text--left">
+      <div className="toss_p_grid toss_typography_p" style={{ marginTop: "50px" }}>
+        <div className="toss_p_grid_col toss_text_left">
           <b>에러메시지</b>
         </div>
-        <div className="p-grid-col text--right" id="message">{`${searchParams.get("message")}`}</div>
-      </div>
-      <div className="p-grid typography--p" style={{ marginTop: "10px" }}>
-        <div className="p-grid-col text--left">
-          <b>에러코드</b>
+        <div className="toss_p_grid_col toss_text_right" id="message">
+          {searchParams.get("message")}
         </div>
-        <div className="p-grid-col text--right" id="code">{`${searchParams.get("code")}`}</div>
       </div>
 
-      
+      <div className="toss_p_grid toss_typography_p" style={{ marginTop: "10px" }}>
+        <div className="toss_p_grid_col toss_text_left">
+          <b>에러코드</b>
+        </div>
+        <div className="toss_p_grid_col toss_text_right" id="code">
+          {searchParams.get("code")}
+        </div>
+      </div>
     </div>
   );
 }
--- src/features/payment/pages/SuccessPage.tsx
+++ src/features/payment/pages/SuccessPage.tsx
@@ -7,21 +7,16 @@ export function WidgetSuccessPage() {
   const [responseData, setResponseData] = useState(null);
 
   useEffect(() => {
-    //서버 결제 확인 로직
     async function confirm() {
-      // URL에서 파라미터를 가져와 requestData 생성
       const requestData = {
         orderId: searchParams.get("orderId"),
         amount: searchParams.get("amount"),
         paymentKey: searchParams.get("paymentKey"),
       };
 
-      // 서버의 /api/confirm/widget로 POST 요청
       const response = await fetch("/api/confirm/widget", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(requestData),
       });
 
@@ -35,45 +30,55 @@ export function WidgetSuccessPage() {
     }
 
     confirm()
-      .then((data) => {
-        setResponseData(data);
-      })
+      .then((data) => setResponseData(data))
       .catch((error) => {
         navigate(`/fail?code=${error.code}&message=${error.message}`);
       });
-  }, [searchParams]);
+  }, [searchParams, navigate]);
 
   return (
     <>
-      <div className="box_section" style={{ width: "600px" }}>
-        <img width="100px" src="https://static.toss.im/illusts/check-blue-spot-ending-frame.png" />
+      <div className="toss_box_section" style={{ width: "600px" }}>
+        <img
+          width="100px"
+          src="https://static.toss.im/illusts/check-blue-spot-ending-frame.png"
+          alt="결제 완료"
+        />
         <h2>결제를 완료했어요</h2>
-        <div className="p-grid typography--p" style={{ marginTop: "50px" }}>
-          <div className="p-grid-col text--left">
+
+        <div className="toss_p_grid toss_typography_p" style={{ marginTop: "50px" }}>
+          <div className="toss_p_grid_col toss_text_left">
             <b>결제금액</b>
           </div>
-          <div className="p-grid-col text--right" id="amount">
+          <div className="toss_p_grid_col toss_text_right" id="amount">
             {`${Number(searchParams.get("amount")).toLocaleString()}원`}
           </div>
         </div>
-        <div className="p-grid typography--p" style={{ marginTop: "10px" }}>
-          <div className="p-grid-col text--left">
+
+        <div className="toss_p_grid toss_typography_p" style={{ marginTop: "10px" }}>
+          <div className="toss_p_grid_col toss_text_left">
             <b>주문번호</b>
           </div>
-          <div className="p-grid-col text--right" id="orderId">
-            {`${searchParams.get("orderId")}`}
+          <div className="toss_p_grid_col toss_text_right" id="orderId">
+            {searchParams.get("orderId")}
           </div>
         </div>
-        <div className="p-grid typography--p" style={{ marginTop: "10px" }}>
-          <div className="p-grid-col text--left">
+
+        <div className="toss_p_grid toss_typography_p" style={{ marginTop: "10px" }}>
+          <div className="toss_p_grid_col toss_text_left">
             <b>paymentKey</b>
           </div>
-          <div className="p-grid-col text--right" id="paymentKey" style={{ whiteSpace: "initial", width: "250px" }}>
-            {`${searchParams.get("paymentKey")}`}
+          <div
+            className="toss_p_grid_col toss_text_right"
+            id="paymentKey"
+            style={{ whiteSpace: "initial", width: "250px" }}
+          >
+            {searchParams.get("paymentKey")}
           </div>
         </div>
       </div>
-      <div className="box_section" style={{ width: "600px", textAlign: "left" }}>
+
+      <div className="toss_box_section" style={{ width: "600px", textAlign: "left" }}>
         <b>Response Data :</b>
         <div id="response" style={{ whiteSpace: "initial" }}>
           {responseData && <pre>{JSON.stringify(responseData, null, 4)}</pre>}
