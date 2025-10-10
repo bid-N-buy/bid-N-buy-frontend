cc1a6ad98d5edc302f109680408fe9db13430809
MA-Ha-eun <1thecalla@gmail.com>
2025-10-10 17:08:33 +0900

    feat: 관리자 페이지 일괄 추가

---
+++ src/features/admin/pages/AdminInquiryList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminInquiryList = () => {
+  return <div>AdminInquiryList</div>;
+};
+
+export default AdminInquiryList;
---
+++ src/features/admin/pages/AdminSignUpPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminSignUpPage = () => {
+  return <div>AdminSignUpPage</div>;
+};
+
+export default AdminSignUpPage;
