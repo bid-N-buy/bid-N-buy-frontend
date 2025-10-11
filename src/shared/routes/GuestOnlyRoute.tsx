99200806485e637aa63c6689f7edc1dc228d2110
MA-Ha-eun <1thecalla@gmail.com>
2025-10-12 00:47:45 +0900

    fix: 라우트 token 을 accessToken 으로 변경

--- src/shared/routes/GuestOnlyRoute.tsx
+++ src/shared/routes/GuestOnlyRoute.tsx
@@ -3,7 +3,7 @@ import { useAuthStore } from "../../features/auth/store/authStore";
 import { Navigate, Outlet } from "react-router-dom";
 
 const GuestOnlyRoute = () => {
-  const token = useAuthStore.getState().token;
+  const token = useAuthStore.getState().accessToken;
   if (token) return <Navigate to="/" replace />;
   return <Outlet />;
 };
--- src/shared/routes/ProtectedRoute.tsx
+++ src/shared/routes/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { useAuthStore } from "../../features/auth/store/authStore";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  const token = useAuthStore.getState().token;
+  const token = useAuthStore.getState().accessToken;
   if (!token) return <Navigate to="/login" replace />;
   return <Outlet />;
 };
