8a5f148f2b0e487c4ef4df73a23e30de29ef110f
MA-Ha-eun <1thecalla@gmail.com>
2025-10-12 23:23:05 +0900

    chore: 개발용 프록시 설정 제거

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        secure: false,
-        // /api 제거
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        // cookieDomainRewrite: "localhost",
-      },
-    },
-  },
 });
