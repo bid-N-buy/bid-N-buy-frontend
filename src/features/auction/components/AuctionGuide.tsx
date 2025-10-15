cb2c94da0637f115f043f4dc821dccaef04d0d53
MA-Ha-eun <1thecalla@gmail.com>
2025-10-16 05:40:41 +0900

    feat: AuctionGuide 레이아웃 수정

--- src/features/auction/components/AuctionGuide.tsx
+++ src/features/auction/components/AuctionGuide.tsx
@@ -2,9 +2,16 @@ import React from "react";
 
 const AuctionGuide = () => {
   return (
-    <div className="bg-g400 h-[232px]" aria-label="추가 정보 영역">
-      중단2
-    </div>
+    <section
+      className="flex min-h-[232px] flex-col gap-1.5 px-1.5 sm:gap-1.5 sm:px-3 md:gap-2 lg:gap-3.5"
+      aria-label="경매 가이드"
+    >
+      <span className="text-g100 text-h5 sm:text-h4 font-bold">
+        경매 가이드
+      </span>
+
+      <p className="text-g200 text-h7 md:text-h6 px-2 leading-relaxed whitespace-pre-line sm:text-base"></p>
+    </section>
   );
 };
 
