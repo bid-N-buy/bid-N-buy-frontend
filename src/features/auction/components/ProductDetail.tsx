435ca3b12206b8b0b8ff9c53ae78066bd475ebe8
MA-Ha-eun <1thecalla@gmail.com>
2025-10-22 02:15:39 +0900

    style: 경매 상품 설명, 연관 상품 ui 수정

--- src/features/auction/components/ProductDetail.tsx
+++ src/features/auction/components/ProductDetail.tsx
@@ -7,13 +7,13 @@ interface ProductDetailProps {
 const ProductDetail = ({ description }: ProductDetailProps) => {
   return (
     <section
-      className="flex min-h-[232px] flex-col gap-1.5 px-1.5 sm:gap-1.5 sm:px-3 md:gap-2 lg:gap-3.5"
+      className="flex min-h-[232px] flex-col gap-1.5 px-1.5 sm:gap-1.5 sm:px-3 md:gap-2 lg:gap-4"
       aria-label="상품 설명 영역"
     >
       <span className="text-g100 text-h5 sm:text-h4 font-bold">상품 설명</span>
 
       {description ? (
-        <p className="text-g200 text-h7 md:text-h6 px-2 leading-relaxed whitespace-pre-line sm:text-base">
+        <p className="text-g100 text-h7 md:text-h6 px-2 leading-relaxed whitespace-pre-line sm:text-base">
           {description}
         </p>
       ) : (
