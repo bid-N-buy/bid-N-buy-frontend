46770409766156d091d866a70adf3b1d7ec989ad
lunainwoozu <run2u7@gmail.com>
2025-10-24 21:30:40 +0900

    feat: 알림 발송 모달 구현 중

--- src/features/admin/components/AdminAlertPostModal.tsx
+++ src/features/admin/components/AdminAlertPostModal.tsx
@@ -1,21 +1,40 @@
 import { useRef, useEffect, useState } from "react";
+import api from "../../../shared/api/axiosInstance";
+import { useAdminAuthStore } from "../store/adminStore";
 import useToast from "../../../shared/hooks/useToast";
 import type { ModalProps } from "../../../shared/types/CommonType";
 import { X } from "lucide-react";
 
 const AdminAlertPostModal = ({ onClose }: ModalProps) => {
   const modalRef = useRef<HTMLDivElement>(null);
-  const [type, setType] = useState("");
+  const adminToken = useAdminAuthStore.getState().accessToken;
   const [content, setContent] = useState("");
 
   // 전송 완료/실패 알림
   const { showToast } = useToast();
 
-  const sendAlert = () => {};
+  const sendAlert = async () => {
+    if (!adminToken) {
+      showToast("토큰이 없습니다.", "error");
+      return;
+    }
+    try {
+      await api.post(`/notifications`, {
+        headers: {
+          Authorization: `Bearer ${adminToken}`,
+        },
+        withCredentials: true,
+      });
+      showToast("알림이 발송되었습니다.", "success");
+    } catch (error) {
+      console.error("알림 발송 실패:", error);
+      showToast("알림 발송에 실패했습니다.", "error");
+      return;
+    }
+  };
 
-  const submitAlert = (e) => {
+  const submitAlert = (e: React.FormEvent) => {
     e.preventDefault(); // 폼 전송 방지
-    showToast("알림이 발송되었습니다", "success");
     sendAlert();
   };
 
@@ -32,13 +51,26 @@ const AdminAlertPostModal = ({ onClose }: ModalProps) => {
 
   return (
     <div
-      className="border-g500 fixed inset-0 z-51 h-full w-full rounded-md border-1 bg-white text-wrap shadow-lg md:absolute md:inset-auto md:top-[72px] md:right-8 md:h-150 md:w-100"
+      className="border-g500 fixed inset-0 z-51 h-full w-full rounded-md border-1 bg-white text-wrap shadow-lg md:absolute md:inset-auto md:top-[72px] md:left-18 md:h-100 md:w-100"
       ref={modalRef}
     >
       <button onClick={onClose} aria-label="채팅 모달 닫기">
         <X />
       </button>
       <form onSubmit={submitAlert}>
+        <label className="text-g100 w-full flex-shrink-0 text-base font-medium">
+          알림 발송
+        </label>
+        <textarea
+          name="alertContent"
+          id="alertContent"
+          placeholder="알림으로 보낼 내용을 작성해 주세요."
+          className="h-full w-full focus:outline-none"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          style={{ resize: "none" }}
+          required
+        />
         <button type="submit">전송</button>
       </form>
     </div>
