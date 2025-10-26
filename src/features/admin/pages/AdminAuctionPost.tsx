fed5061e6e7df3c7169436c9aae7bef78272b341
run2u7@gmail.com <run2u7@gmail.com>
2025-10-27 01:42:21 +0900

    feat: 관리자 루트 추가 및 리스트, 포스트 파일 추가

---
+++ src/features/admin/pages/AdminAuctionPost.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminAuctionPost = () => {
+  return <div></div>;
+};
+
+export default AdminAuctionPost;
---
+++ src/features/admin/pages/AdminUserPost.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminUserPost = () => {
+  return <div></div>;
+};
+
+export default AdminUserPost;
