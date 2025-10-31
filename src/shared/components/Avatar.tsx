f018f682b120299a0311f0ccabe1788a5407d909
lunainwoozu <run2u7@gmail.com>
2025-10-31 19:49:34 +0900

    style: 기본 이미지 설정 수정

--- src/shared/components/Avatar.tsx
+++ src/shared/components/Avatar.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import type { AvatarProps } from "../types/CommonType";
-import { profile_default } from "../types/CommonType";
+import avatar from "../../assets/avatar.svg";
+import { buildImageUrl } from "../utils/imageUrl";
 
 const Avatar = ({ imageUrl, nickname }: AvatarProps) => {
   return (
     <div className="size-12 overflow-hidden rounded-full">
-      <img
-        src={imageUrl ? imageUrl : profile_default}
-        alt={`${nickname}의 프로필 사진`}
-      />
+      {
+        <img
+          src={imageUrl ? buildImageUrl(imageUrl) : avatar}
+          alt={`${nickname}의 프로필 사진`}
+        />
+      }
     </div>
   );
 };
