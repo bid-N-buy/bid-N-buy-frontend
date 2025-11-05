7588aea670a61942e68b0cb1f02f67e6118031a2
sweetpotatotest <kgb4232@daum.net>
2025-11-05 11:45:57 +0900

    fix: 오류수정

--- src/features/notification/pages/NotiList.tsx
+++ src/features/notification/pages/NotiList.tsx
@@ -8,44 +8,43 @@ import { useChatModalStore } from "../../../shared/store/ChatModalStore";
 import type { NotiModalProps } from "../types/NotiType";
 
 const NotiList = ({ notis, onChatAdd }: NotiModalProps) => {
-  
+
   const token = useAuthStore((s) => s.accessToken);
   const { makeChatRoomInAuc, openChatRoom } = useChatModalStore();
 
-  const handleChatAdd = async (auctionId: number, sellerId: number) => {
-    if (!token) return;
-
-  return (
-    <ul className="h-full">
-      {notis.length === 0 && (
-        <div className="text-g300 flex h-full items-center justify-center text-sm">
-          알림이 없습니다.
-        </div>
-      )}
-      {notis.map((noti) => (
-        <li
-          key={noti.notificationId}
-          className={`border-g400 flex gap-2 border-b p-4 hover:bg-gray-50 ${noti.content.length < 27 && `items-center`}`}
-        >
-          {noti.type.toLowerCase() === "alert" ? (
-            <Bell />
-          ) : noti.type.toLowerCase() === "notice" ? (
-            <Megaphone />
-          ) : noti.type.toLowerCase() === "auction_result" ? (
-            <MessageCircleMore />
-          ) : (
-            <TriangleAlert />
-          )}
-          <div className="w-[90%]">
-            <p className="mb-1 text-xs text-gray-400">
-              {formatTime(noti.createdAt)}
-            </p>
-            <p>{noti.content}</p>
+  const notiList = ({ notis }: NotiModalProps) => {
+    return (
+      <ul className="h-full overflow-y-auto">
+        {notis.length === 0 && (
+          <div className="text-g300 flex h-full items-center justify-center text-sm">
+            알림이 없습니다.
           </div>
-        </li>
-      ))}
-    </ul>
-  );
-};
+        )}
+        {notis.map((noti) => (
+          <li
+            key={noti.notificationId}
+            className={`border-g400 flex gap-2 border-b p-4 hover:bg-gray-50 ${noti.content.length < 27 && `items-center`}`}
+          >
+            {noti.type.toLowerCase() === "alert" ? (
+              <Bell />
+            ) : noti.type.toLowerCase() === "notice" ? (
+              <Megaphone />
+            ) : noti.type.toLowerCase() === "auction_result" ? (
+              <MessageCircleMore />
+            ) : (
+              <TriangleAlert />
+            )}
+            <div className="w-[90%]">
+              <p className="mb-1 text-xs text-gray-400">
+                {formatTime(noti.createdAt)}
+              </p>
+              <p>{noti.content}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+}
 
 export default NotiList;
