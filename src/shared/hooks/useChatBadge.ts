8cba6a1ab924be586d2aa8014c217b9ff21f052d
lunainwoozu <run2u7@gmail.com>
2025-11-10 22:48:24 +0900

    fix: 채팅 알림 뱃지 전역 처리 수정

--- src/shared/hooks/useChatBadge.ts
+++ src/shared/hooks/useChatBadge.ts
@@ -4,10 +4,11 @@ import api from "../api/axiosInstance";
 import SockJS from "sockjs-client";
 import { useAuthStore } from "../../features/auth/store/authStore";
 import { useChatModalStore } from "../store/ChatModalStore";
+import type { ChatListItemProps } from "../../features/chatting/types/ChatType";
 
 export const useChatBadge = () => {
   const token = useAuthStore((s) => s.accessToken);
-  const totalUnreadCount = useChatModalStore((s) => s.totalUnreadCount);
+  // const totalUnreadCount = useChatModalStore((s) => s.totalUnreadCount);
   const clientRef = useRef<Client | null>(null);
 
   useEffect(() => {
@@ -21,16 +22,19 @@ export const useChatBadge = () => {
         // 서버에 있는 모든 방 구독
         // 서버가 어떤 방에 내가 속했는지 push 안 해주므로 구독 목록 구성
         api
-          .get("/chatrooms/list", {
+          .get<ChatListItemProps[]>("/chatrooms/list", {
             headers: { Authorization: `Bearer ${token}` },
           })
           .then((res) => {
             // room 부분 수정했으므로 배포 후 다시 볼 것
-            res.data.forEach((room: any) => {
-              client.subscribe(`/topic/chat/room/${room.chatroomId}`, (msg) => {
-                const data = JSON.parse(msg.body);
-                useChatModalStore.getState().handleNewChatMessage(data);
-              });
+            res.data.forEach((chatRoom) => {
+              client.subscribe(
+                `/topic/chat/room/${chatRoom.chatroomId}`,
+                (msg) => {
+                  const data = JSON.parse(msg.body);
+                  useChatModalStore.getState().handleNewChatMessage(data);
+                }
+              );
             });
           });
       },
@@ -43,5 +47,5 @@ export const useChatBadge = () => {
       client.deactivate();
       clientRef.current = null;
     };
-  }, [token, totalUnreadCount]);
+  }, [token]);
 };
