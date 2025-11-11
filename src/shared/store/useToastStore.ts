f8871443fb96608cd110b64663729f05717f44da
lunainwoozu <run2u7@gmail.com>
2025-11-11 16:13:26 +0900

    fix: 토스트 zustand로 전역 관리하게끔 수정

---
+++ src/shared/store/useToastStore.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+
+interface ToastState {
+  isVisible: boolean;
+  message: string;
+  type: "success" | "error";
+
+  showToast: (message: string, type: "success" | "error") => void;
+  hideToast: () => void;
+}
+
+export const useToastStore = create<ToastState>((set) => ({
+  isVisible: false,
+  message: "",
+  type: "success",
+
+  showToast: (message, type = "success") =>
+    set({
+      isVisible: true,
+      message,
+      type,
+    }),
+
+  hideToast: () => set({ isVisible: false, message: "", type: "success" }),
+}));
